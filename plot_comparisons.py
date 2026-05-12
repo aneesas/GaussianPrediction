145c1c08920f156fad108e5661c408a5c48a370b
Aneesa Sonawalla <9471211+aneesas@users.noreply.github.com>
2026-05-12 14:18:49 -0400

    Inputting metrics to plot

---
+++ plot_comparisons.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+GP_JUMPING_LPIPS = []
+HN_JUMPING_LPIPS = np.array([0, 0.0444, 0.0389, 0.0391, 0.0507, 0.0805])
+
+GP_JUMPING_PSNR = []
+HN_JUMPING_PSNR = np.array([0, 38.54, 37.35, 34.98, 34.07, 26.30])
+
+GP_JUMPING_SSIM = []
+HN_JUMPING_SSIM = np.array([0, 0.9952, 0.9939, 0.9898, 0.9881, 0.9506])
+
+GP_BALLS_LPIPS = []
+HN_BALLS_LPIPS = np.array([0, 0.0594, 0.0631, 0.0579, 0.0367, 0.0449])
+
+GP_BALLS_PSNR = []
+HN_BALLS_PSNR = np.array([0, 41.16, 41.82, 38.69, 39.38, 32.93])
+
+GP_BALLS_SSIM = []
+HN_BALLS_SSIM = np.array([0, 0.9913, 0.9946, 0.9880, 0.9909, 0.9757])
